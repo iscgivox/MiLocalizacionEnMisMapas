6a761b9d80e39902d4b97ba269894493ced494e5
iscgivox <isc.givox@gmail.com>
2016-10-10 14:12:57 -0500

    Se agrego este elemento principal para  crear un mapa

---
+++ app/src/main/java/net/ivanvega/mismapas/ActivityMapas.java
@@ -0,0 +1,42 @@
+package net.ivanvega.mismapas;
+
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ActivityMapas extends AppCompatActivity
+implements OnMapReadyCallback
+{
+    SupportMapFragment mapFragment ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapas);
+
+//        mMapFragment = MapFragment.newInstance();
+//        FragmentTransaction fragmentTransaction =
+//                getFragmentManager().beginTransaction();
+//        fragmentTransaction.add(R.id.my_container, mMapFragment);
+//        fragmentTransaction.commit();
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+
+    }
+
+}
